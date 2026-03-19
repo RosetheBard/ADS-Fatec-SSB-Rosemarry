57f64a6c085ddf662df59813d6938197f96ca846
RosetheBard <rothart26@gmail.com>
2026-03-19 13:07:21 -0300

    portifolio de desenvolvimento web

---
+++ adsROSEMARRY/aulas-que-passaram/030326/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css">
+    <title>Aula 03 de março</title>
+</head>
+<body>
+    <div class="tudo">
+        <div class="topo">
+            <div class="logo">
+                <img src="img/lua.png" alt="simbolo de lua com estelas de quatro pontas" class="logotipo">
+            </div>
+
+            <div class=form>
+                <form action="">
+                    <input type="text"  placeholder="seu usuario">
+                    <input type="text"  placeholder="senha">
+                </form>
+            </div>
+        </div>
+        <div class="menu">
+            <ul>
+                <li>TESTE</li>
+                <li>TESTE</li>
+                <li>TESTE</li>
+                <li>TESTE</li>
+                <li>TESTE</li>
+            </ul>
+        </div>
+        <div class="lateral">
+        </div>
+        <div class="meio">
+        </div>
+        <div class="rodape">
+        </div>
+    </div>
+</body>
+</html>
---
+++ adsROSEMARRY/aulas-que-passaram/100326/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>10 de março de 2026</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <link rel="shortcut icon" href="img/lua.png" type="image/x-icon">
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 bg-dark-subtle">
+                <img src="img/lua.png" alt="icone de lua" clas="img-fluid float-left" style="height: 100px;">
+
+                <div class="d-flex flex-row-reverse bd-highlight">
+                    <div class="p-1 bd-highlight">Contato</div>
+                    <div class="p-1 bd-highlight">Projetos</div>
+                    <div class="p-1 bd-highlight">Perfil</div>
+                </div>
+
+            </div>
+
+            <div class="col-12 bg-body-secondary" >
+
+                
+
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
