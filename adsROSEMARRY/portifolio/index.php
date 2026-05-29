cd1d4edeb692526053b959d38718ba13e0162485
RosetheBard <rothart26@gmail.com>
2026-05-28 21:54:13 -0300

    refiz todo o portifolio

--- adsROSEMARRY/portifolio/index.php
+++ adsROSEMARRY/portifolio/index.php
@@ -13,55 +13,27 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playwrite+GB+J:ital,wght@0,100..400;1,100..400&display=swap" rel="stylesheet">
 
     <title>Rose-HomePage</title>
 </head>
 
 <body class="conteiner">
-    <header class="sticky-top row">
-        <nav id="navbar-example" class="navbar p-3">
-            <a class="navbar-brand" href="#"><img src="img/lua.png"
-                    alt="icone em forma de lua preta, representando a logo do portifolio" class="i-sm"></a>
-            <ul class="nav nav-pills">
-                <li class="nav-item">
-                    <a class="nav-link" href="#scrollspyHeading1">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#scrollspyHeading2">Habilidades</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#scrollspyHeading3">Prpjetos</a>
-                </li>
-                <li class="nav-item">
-                    <button class="btn" type="button" data-bs-toggle="offcanvas"
-                        data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">...</button>
-                </li>
-            </ul>
+    <header class="row p-1">
+        <nav>
+            <img src="img/lua.png" alt="" class="i-sm ms-2">
         </nav>
-
-        <div class="offcanvas offcanvas-end bx-pink p-2" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1"
-            id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
-            <div class="offcanvas-header">
-                <h5 class="offcanvas-title" id="offcanvasScrollingLabel">Offcanvas with body scrolling</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-            </div>
-            <div class="offcanvas-body">
-                <p>Try scrolling the rest of the page to see this option in action.</p>
-            </div>
-        </div>
-
     </header>
 
     <main class="d-flex row">
-        <!-- conteudo do portifolio -->
-        <div data-bs-spy="scroll" data-bs-target="#navbar-example2" data-bs-root-margin="0px 0px -40%"
-            data-bs-smooth-scroll="true" class="scrollspy-example bx-pink p-3 rounded-2 text-center m-1" tabindex="0">
-            <h2 id="scrollspyHeading1">Quem sou eu?</h2>
-            <p>...</p>
-            <h2 id="scrollspyHeading2">Habilidades</h2>
-            <p>...</p>
-            <h2 id="scrollspyHeading3">Ultimos Projetos</h2>
-            <p>...</p>
+        <div class="bx pt-5 text-center">
+            <h1 class="playwrite-gb-j fs-2">Olá, Seja Bem Vindo ao meu Portifolio</h1>
+            <p class="playwrite-gb-j fs-1 fw-bold">Sou Rosemarry, mas pode me chamar de Marry</p>
+
+            <img src="img/pfbanner-2.jpg" alt="" class="m-3 w-50">
+
         </div>
     </main>
 
